392c49c0723b178e16d9a71b7e2d6438487d1522
Hana Sheto <has6447@truman.edu>
2022-08-02 15:55:11 +0000

    getting input from the user

---
+++ endlessMedical.py
@@ -0,0 +1,90 @@
+import requests
+from requests.structures import CaseInsensitiveDict
+
+yn_questions = {"AbdCramps":"Abdominal cramps","Chills": "Chills","ChestPainAnginaYesNo":"Chest Pain", "Conjunctivas":"Conjunctivas",\
+                "DecreasedMood":"Decreased Mood",\
+                "Constipation":"Constipation","EatingPain":"Eating Pain","GeneralizedFatigue":"Fatigue",\
+                "HeadacheFrontal":"Headahce","ResistantHypertension":"Hypertension","HypoTension":"HypoTension",\
+                "EyesItchy":"Itching eyes","Nocturia":"Nocturia","Nausea":"Nausea", "NoseCongestion": "Nose congestion",\
+                "LossOfSmell":"Loss of smell","LossOfTaste":"Loss of taste","LossOfConsciousness":"Loss of consciousness",\
+                 "LowbackPain":"Low back pain","Seizure":"Seizure","Snoring":"Snoring","Sneezing":"Sneezing",\
+                "SwallowPain":"Pain when swallowing","Vomiting":"Vomiting","WeightLoss":"Weight loss","Weight gain":"Weight gain"}
+
+num_questions = {"Temp":"Temperature"}
+
+def getSessionId():
+    """Get the session and confirmation to use the EndlessMedical API"""
+    url = "https://api.endlessmedical.com/v1/dx/InitSession"
+    sessionID = requests.get(url).json()['SessionID']
+    confirm =  "I have read, understood and I accept and agree to comply with the Terms of Use of EndlessMedicalAPI and Endless Medical services. The Terms of Use are available on endlessmedical.com"
+    requests.post("https://api.endlessmedical.com/v1/dx/AcceptTermsOfUse?SessionID="+sessionID+"&passphrase="+confirm)
+    return sessionID
+
+def addSymptoms(sessionID,name,value):
+    """Create the symptoms string"""
+    requests.post("https://api.endlessmedical.com/v1/dx/UpdateFeature?SessionID="+sessionID+"&name="+name+"&value="+value)
+    
+
+
+def Analyze(sessionID):
+    """Analyze and return the possible disease"""
+    return requests.get("https://api.endlessmedical.com/v1/dx/Analyze?SessionID="+sessionID+"&NumberOfResults=5").json()
+
+def getDiseases(data):
+    """Extract all diseases of the analyze and store them in an array"""
+    dictio = data["Diseases"]
+    diseases = []
+    for disease in dictio:
+        diseases.append(*disease)
+    return diseases
+
+def getCoordinates(zip_c):
+    weather_url = 'https://api.weatherapi.com/v1/current.json'
+    weather_query = {"q": zip_c}
+    weather_headers = {
+        'key': 'e7e5a0eed9044fbab81184925222907'
+    }
+
+    data = requests.get(weather_url, headers=weather_headers,
+                                params=weather_query).json()
+    coordinates = data['location']
+    return [coordinates['lon'],coordinates['lat']]
+
+def filter(sessionID):
+    dictio = requests.get("https://api.endlessmedical.com/v1/dx/GetSuggestedSpecializations?SessionID="+\
+                    sessionID+"&NumberOfResults=10").json()
+    specializations = []
+    for specialization in dictio['SuggestedSpecializations']:
+        specializations.append(specialization[0])
+    return specializations
+
+def getCategories(specialization):
+    
+
+ def suggestHospital(coordinates,category):
+
+    dist = 10*1609.344
+    headers = CaseInsensitiveDict()
+    headers["Accept"] = "application/json"
+    main_url = "https://api.geoapify.com/v2/places?"
+    category_url = "categories="+category
+    coord_url = "&filter=circle:" + str(coordinates[0]) + \
+                "," + str(coordinates[1]) + "," + str(dist)
+    limit_url = "&limit="+str(5)
+    api_key = "&apiKey=a81ef38a8fdb430c9ff29347d1ed7825"
+    url = main_url+category_url+coord_url+limit_url+api_key
+    resp = requests.get(url,headers)
+    return resp.json()
+
+if __name__ == "__main__":
+    #print(suggestHospital(getCoordinates(65201),"healthcare"))
+    sessionID = getSessionId()
+    addSymptoms(sessionID,"Temp","90")
+    addSymptoms(sessionID,"Fasting","0")
+    addSymptoms(sessionID,"Constipation","0")
+    #addSymptoms(sessionID,"Vomiting","0")
+    #addSymptoms(sessionID,"HeartBurn","0")
+    #addSymptoms(sessionID,"AbdCramps","0")
+    #addSymptoms(sessionID,"Nausea","0")
+    #print(getDiseases(Analyze(sessionID)))
+    print(filter(sessionID))
---
+++ routes.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, url_for, flash, redirect , request
+from forms import RegistrationForm, LoginForm
+from flask_behind_proxy import FlaskBehindProxy
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+
+proxied = FlaskBehindProxy(app)  ## add this line
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'eae4d31ce5ddcbd006c3fca0d8183dd2'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(20), unique=True, nullable=False)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+  password = db.Column(db.String(60), nullable=False)
+
+  def __repr__(self):
+    return f"User('{self.username}', '{self.email}')"
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit(): # checks if entries are valid
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('login')) # if so - send to home page
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login",methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html',title='Log In',form=form)
+
+@app.route("/abdominalchecker", methods = ['GET','POST'])# this tells you the URL the method below is related to
+def abdominalchecker():
+    if request.method == 'POST':
+        print(request.form.getlist('mycheckbox'))
+        return 'Done'
+    return render_template('home.html')
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
