7fddf17b505dc52a9ddac3e3808313fa99643ca9
Hana Sheto <has6447@truman.edu>
2022-08-02 21:01:42 +0000

    Updated database

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-
+from datetime import datetime, date
 from flask import Flask, render_template, url_for, flash, redirect , request
 from forms import RegistrationForm, LoginForm
 from flask_behind_proxy import FlaskBehindProxy
@@ -11,8 +11,21 @@ proxied = FlaskBehindProxy(app)  ## add this line
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'eae4d31ce5ddcbd006c3fca0d8183dd2'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///userdata.db'
 db = SQLAlchemy(app)
+
+# model that define what will be included in the database
+# db.model is the way that shows how our database will look like
+
+class Symptomtrack(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.String(30),  nullable= False)
+    date = db.Column(db.String(30),  nullable= False)
+    #need to check wheather it should be null or not 
+    symptom = db.Column(db.Text)
+    # this is a magic method 
+    def __repr__(self):
+        return f"User('{self.time}', '{self.date}', '{self.symptom}')" 
 
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
@@ -48,7 +61,21 @@ def login():
 @app.route("/abdominalchecker", methods = ['GET','POST'])# this tells you the URL the method below is related to
 def abdominalchecker():
     if request.method == 'POST':
-        print(request.form.getlist('mycheckbox'))
+        items = request.form.getlist('mycheckbox')
+        #change the user_symptom list to string and use it symtom text
+        user_symptom = ""
+        for item in items:
+            user_symptom += str(item) + " "
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        #date = now.day()
+        #print(current_time)
+        #day
+        today1 = date.today()
+        #print(today1)
+        symptom = Symptomtrack(time= str(current_time) , date= str(today1), symptom = user_symptom)
+        db.session.add(symptom)
+        db.session.commit()
         return 'Done'
     return render_template('home.html')
 
